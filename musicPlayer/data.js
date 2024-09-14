41619ed3682d42ef5b3762311e9e78fa6b0ae464
SiwarMrika <161029766+SiwarMrika@users.noreply.github.com>
2024-09-14 12:23:03 +0100

    Add files via upload

---
+++ musicPlayer/data.js
@@ -0,0 +1,59 @@
+let songs=[
+    {
+        name:'Rolling in the Deep',
+        artist:'Adele',
+        path:'/music/ssi.mp3juice.blog - Adele - Rolling in the Deep (Official Music Video).mp3',
+        cover:'/img/rolling.jpg'
+    },
+
+    {
+        name:'Someone Like You',
+        artist:'Adele',
+        path:'/music/ssi.mp3juice.blog - Adele - Someone Like You (Official Music Video).mp3',
+        cover:'/img/maxresdefault.jpg'
+    },
+
+    {
+        name:'Set Fire to the Rain',
+        artist:'Adele',
+        path:'/music/ssi.mp3juice.blog - Set Fire to the Rain.mp3',
+        cover:'/img/setfire.jfif'
+    },
+
+    {
+        name:'Blank Space',
+        artist:'Taylor Swift',
+        path:'/music/ssi.mp3juice.blog - Taylor Swift - Blank Space.mp3',
+        cover:'/img/Taylor-Swift-blank-space.jpg'
+    },
+
+    {
+        name:'Daylight',
+        artist:'Taylor Swift',
+        path:'/music/ssi.mp3juice.blog - Taylor Swift - Daylight (Official Audio).mp3',
+        cover:'/img/daylight1.png'
+    },
+
+    {
+        name:'Style',
+        artist:'Taylor Swift',
+        path:'/music/ssi.mp3juice.blog - Taylor Swift - Style.mp3',
+        cover:'/img/style.jpg'
+    },
+
+    {
+        name:'SHOULD I',
+        artist:'Rilés',
+        path:'/music/ssi.mp3juice.blog - Rilès - SHOULD I (Music Video).mp3',
+        cover:'/img/shouldI.jpg'
+    },
+
+    {
+        name:'After the Darkness',
+        artist:'Rilés',
+        path:'/music/ssi.mp3juice.blog - Rilès - After the Darkness (Prod. Rilès).mp3',
+        cover:'/img/afterthedarkness.jpg'
+    },
+
+
+]
